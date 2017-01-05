21875aabe2db6dea0b95184d55d0f96d802d85c8
Nick Xie <nicholasxie@v1w-odd-172-31-228-149.rn.uwaterloo.ca>
2017-01-05 00:02:59 -0500

    Very preliminary Player and Game creation through user input

--- avaron/urls.py
+++ avaron/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from . import views
 from .models import Game
-
+app_name='avaron'
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(?P<game_room_num>[0-9]+)/$', views.game_room), #Actual game room, directed from game being made
-    url(r'^game/(?P<game_num>[0-9]+)/$', views.make_game), #Makes game, directed from make_player
-    url(r'^(?P<player_name>[\w\-]+)/(?P<game_num>[0-9]+)/$', views.make_player), #Makes player, directs to make_game
+    url(r'^(?P<game_room_num>[0-9]+)/$', views.game_room, name='game_room'), #Actual game room, directed from make_player
+    url(r'^game/(?P<game_num>[0-9]+)/$', views.make_game), #Makes game, directed from make_player EDIT: now obsolete
+    url(r'^make_player/$', views.make_player, name='make_player'), #makes player and game
+    #url(r'^(?P<player_name>[\w\-]+)/(?P<game_num>[0-9]+)/$', views.make_player), #Makes player, directs to make_game
 ]
