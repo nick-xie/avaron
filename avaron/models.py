ef0f71f23aaa6634f3f88ab8f4756a6b04e31bb9
Nick Xie <sillyfrabbit@yahoo.ca>
2017-02-19 18:07:48 -0500

    Game rules added. At expense of readable code

--- avaron/models.py
+++ avaron/models.py
@@ -6,12 +6,17 @@ class Game(models.Model):
 	room_num = models.IntegerField()
 	pub_date = models.DateTimeField('date created')
 	game_started = models.BooleanField(default=0)
+	rules_v = models.CharField(max_length=200, default='[]') #visibility
+	rules_t = models.CharField(max_length=200, default='[]') #team arrangements
+	rules_wl = models.CharField(max_length=200, default='[]') #win lose conditions, i.e. mission details
 	def __str__(self):
 		return "Room num: " + str(self.room_num)
 class Player(models.Model):
 	game = models.ForeignKey(Game, on_delete=models.CASCADE)
 	name = models.CharField(max_length=20)
-	role = models.BooleanField()
+	role = models.IntegerField()
 	seed = models.BigIntegerField(default = 0)
+	visible = models.CharField(max_length = 200, default='[]') #the guys this guy can see
+	team = models.CharField(max_length=20, default='na') #which team
 	def __str__(self):
 		return "Name: " + self.name +" Game: " + str(self.game.room_num) + "Seed: " + str(self.seed)
--- avaron/views.py
+++ avaron/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from .forms import PlayerForm
 from django.shortcuts import get_object_or_404
 from random import randint
+import random
 import math
 import json
 #TODO
@@ -84,7 +85,10 @@ def make_game(request):
 		for game in gamelist:
 			roomnums.append(int(game.room_num)) #create an array of all existing game_num's
 		new_num=1+max(roomnums) #simply add 1 to largest current game_num and this is our new game_num
-		g=Game.objects.create(room_num=new_num,pub_date=timezone.now(),game_started=0)
+		visibilityR=[[[1,2],[2,1],[3,1,2],[4],[5]],[[1,2],[2,1],[3,1],[4],[5],[6]],[[1,2,3],[2,1,3],[3,1,2],[4,2,5],[5,2,3],[6],[7]]]
+		teamR=[[[1,2],[3,4,5]],[[1,2],[3,4,5,6]],[[1,2,3],[4,5,6,7]]]
+		g=Game.objects.create(room_num=new_num,pub_date=timezone.now(),
+			game_started=0,rules_v=visibilityR,rules_t=teamR)
 		g.save() #creates game
 		seed=randint(1,9001)
 		p=Player.objects.create(game=g,name=player_name,role=0,seed=seed)
@@ -107,28 +111,53 @@ def send_players(request):
 		return HttpResponse(json.dumps(data),content_type='application/json')
 #in game
 def start_game(request, game_num, round_num):
-	g=Game.objects.filter(room_num=game_num)
+	g=Game.objects.filter(room_num=game_num).first()
 	players = Player.objects.filter(game=g).order_by('seed') #players in game, sorted
-	num_bad=math.floor(players.count()*0.43)
-	i=1 #counter
-	if int(g.first().game_started)==0: #first person to press start game
-		for player in players: #assign roles to players
-			if (i<=num_bad):
-				player.role=0
-			else:
-				player.role=1
+	if int(g.game_started)==0: #first person to press start game
+		visibR=eval(str(g.rules_v)) #come back to this one day
+		teamR=eval(str(g.rules_t))
+		for i in range(0,len(visibR)): #choose appropriate game based off player count
+			print len(visibR[i])
+			if(players.count()==len(visibR[i])):
+				visibR=visibR[i]
+				teamR=teamR[i]
+				break
+		i=1#counter
+		for player in players: #assign roles(num) and team to players
+			player.role=i #player is number i
+			for tc in range(0,len(teamR)): #assigns team
+				if (i in teamR[tc]):
+					player.team=tc
+					break
+			vc=0
+			for vc in range(0, len(visibR)): #give visibility rules to player by vc
+				if (visibR[vc][0]==i):
+					player.visible=visibR[vc] #convert back to string
 			i=i+1
 			player.save()
-	for game in g:
-		game.game_started=1
-		game.save()
+		for player in players: #replace vc with names
+			newVisib=[]
+			for i in range(0,len(player.visible)):
+				#find the name
+				namedP = Player.objects.filter(role=player.visible[i],game=g)
+				newVisib.append(namedP.first().name)
+			random.shuffle(newVisib)
+			player.visible=newVisib
+			player.save()
+		g.game_started=1
+		g.save()
 	our_seed = request.session['id']
-	your_guy=Player.objects.filter(seed=our_seed)
+	your_guy=Player.objects.filter(seed=our_seed).first()
+	formatVis=""
+	for i in eval(str(your_guy.visible)):
+		formatVis=formatVis+str(i)+"\n"
+	print(formatVis)
 	template = loader.get_template('avaron/ingame.html')
 	context = {
 		'players': players,
-		'your_guy': your_guy.first(),
+		'your_guy': your_guy,
 		'game': game_num,
 		'round_num': round_num,
+		'visible': formatVis,
 	}
 	return HttpResponse(template.render(context,request))
